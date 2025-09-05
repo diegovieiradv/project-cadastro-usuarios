fc64c0d6411e3007cd4663848edf47fe0584a9d2
diegovieiradv <dvdiego87@gmail.com>
2025-09-05 00:00:52 -0300

    adicionei o globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import { Title } from './styles';
+import {
+  Title,
+  Container,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  Button,
+  InputLabel,
+} from "./styles";
 
-function App() {
+function Home() {
   return (
-    <div>
-      <Title>Vite + React</Title>
-    </div>
+    <Container>
+      <TopBackground>
+        <img src="" alt="" />
+      </TopBackground>
+      <form>
+        <Title>Cadastrar Usuário</Title>
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <input type="text" placeholder="Nome do usuário" />
+            </div>
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <input type="number" placeholder="Idade do usuário" />
+            </div>
+          </div>
+          <div>
+            <InputLabel>
+              E-mail<span>*</span>
+            </InputLabel>
+            <input type="email" placeholder="E-mail do usuário" />
+          </div>
+        </ContainerInputs>
+        <button>Cadastrar Usuário</button>
+      </form>
+    </Container>
   );
 }
 
-export default App;
+export default Home;
--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: #333;
-  margin-bottom: 16px;
-`;
+export const Container = styled.div``;
+export const TopBackground = styled.div``;
+export const Form = styled.form``;
+export const ContainerInputs = styled.div``;
+export const Input = styled.input``;
+export const InputLabel = styled.label``;
+export const Button = styled.button``;
+export const Title = styled.h1``;
