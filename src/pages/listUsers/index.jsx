29b539155f7b5b57d0ec9397b1b3299ef5db1651
diegovieiradv <dvdiego87@gmail.com>
2025-09-14 23:08:27 -0300

    estilizei segunda tela e avatar no projetct

--- src/pages/listUsers/index.jsx
+++ src/pages/listUsers/index.jsx
@@ -3,7 +3,14 @@ import TopBackground from "../../components/TopBackground";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Api from "../../services/api.js";
-import { Title } from "../Home/styles.js";
+import { Container, Title } from "../Home/styles.js";
+import {
+  ContainerUsers,
+  CardUsers,
+  AvatarUser,
+  TrashIcon,
+  DeleteUser,
+} from "./styles.js";
 
 function ListUsers() {
   const [users, setUsers] = useState([]);
@@ -18,23 +25,28 @@ function ListUsers() {
 
   const navigate = useNavigate();
   return (
-    <div>
+    <Container>
       <TopBackground />
       <Title>Lista de Usuários</Title>
       <ContainerUsers>
         {users.map((user) => (
           <CardUsers key={user.id}>
+            <AvatarUser
+              src={`https://avatar.iran.liara.run/public?username=${user.id}`}
+            />
             <div>
               <p>{user.name}</p>
               <p>{user.email}</p>
               <p>{user.age}</p>
             </div>
-            <TrashIcon onClick={() => deleteUser(user.id)} />
+            <TrashIcon onClick={() => DeleteUser(user.id)} />
           </CardUsers>
         ))}
       </ContainerUsers>
-      <Button onClick={() => navigate("/")}>Voltar </Button>
-    </div>
+      <Button type="button" onClick={() => navigate("/")}>
+        Voltar{" "}
+      </Button>
+    </Container>
   );
 }
 
---
+++ src/pages/listUsers/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const TopBackground = styled.div``;
+
+export const Title = styled.h1``;
+
+export const ContainerUsers = styled.div``;
+
+export const CardUsers = styled.div``;
+
+export const AvatarUser = styled.img``;
+
+export const TrashIcon = styled.img``;
+
+export const DeleteUser = styled.button``;
